ffb2ed0bdfd32dcbf0b0024ce32cdde4e3aba3f0
first_project <1242680852@qq.com>
2020-01-14 09:32:22 +0800

    idea提交github测试

---
+++ src/main/java/user/controller/TestController.java
@@ -0,0 +1,15 @@
+package user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class TestController {
+
+    @GetMapping("/info")
+    public String getInfo(){
+        return "hello";
+    }
+}
